487c4776a781c8adf4116f3bb418cd7f4e2c3744
JoshHalvorson <joshhalvorson16@gmail.com>
2019-10-14 12:43:15 -0700

    Add properties to cpu

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.registers = [0] * 8
+        self.memory = [0] * 256
 
     def load(self):
         """Load a program into memory."""
